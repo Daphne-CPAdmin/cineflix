2a33eee46349af5826736e5e0978f47b6c3f9dfd
rainarit <rraina@ucsd.edu>
2019-10-24 01:13:43 -0700

    Initial Commit

---
+++ CineFlix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CineFlix
+//
+//  Created by Ritik Raina on 10/24/19.
+//  Copyright © 2019 rraina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
