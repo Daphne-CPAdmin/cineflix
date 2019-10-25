fa7108f73a2b3094fcf716510a17b80d09d7b70c
rainarit <rraina@ucsd.edu>
2019-10-24 23:02:47 -0700

    Unit 2 Update!

---
+++ CineFlix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  CineFlix
+//
+//  Created by Ritik Raina on 10/24/19.
+//  Copyright © 2019 rraina. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
